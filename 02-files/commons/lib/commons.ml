e6728ac5c2566a90c830e2a8bab5cf44e3a4af11
Henry Till <henrytill@gmail.com>
2017-01-02 05:41:50 -0500

    02-files: add commons

---
+++ 02-files/commons/lib/commons.ml
@@ -0,0 +1,65 @@
+open Unix
+
+exception End_of_file
+
+type in_channel =
+  { in_buffer: string;
+    in_fd: file_descr;
+    mutable in_pos: int;
+    mutable in_end: int }
+
+let buffer_size = 8192
+
+let open_in filename =
+  { in_buffer = Bytes.create buffer_size;
+    in_fd = openfile filename [O_RDONLY] 0;
+    in_pos = 0;
+    in_end = 0 }
+
+let input_char chan =
+  if chan.in_pos < chan.in_end then
+    begin
+      let c = chan.in_buffer.[chan.in_pos] in
+      chan.in_pos <- chan.in_pos + 1;
+      c
+    end
+  else
+    begin
+      match read chan.in_fd chan.in_buffer 0 buffer_size with
+      | 0 -> raise End_of_file
+      | r ->
+        chan.in_end <- r;
+        chan.in_pos <- 1;
+        chan.in_buffer.[0];
+    end
+
+let close_in chan =
+  close chan.in_fd
+
+type out_channel =
+  { out_buffer: string;
+    out_fd: file_descr;
+    mutable out_pos: int }
+
+let open_out filename =
+  { out_buffer = Bytes.create buffer_size;
+    out_fd = openfile filename [O_WRONLY; O_TRUNC; O_CREAT] 0o666;
+    out_pos = 0 }
+
+let output_char chan c =
+  if chan.out_pos < Bytes.length chan.out_buffer then
+    begin
+      Bytes.set chan.out_buffer chan.out_pos c;
+      chan.out_pos <- chan.out_pos + 1
+    end
+  else
+    begin
+      ignore (write chan.out_fd chan.out_buffer 0 chan.out_pos);
+      Bytes.set chan.out_buffer 0 c;
+      chan.out_pos <- 1
+    end
+
+let close_out chan =
+  ignore (write chan.out_fd chan.out_buffer 0 chan.out_pos);
+  close chan.out_fd
+;;
