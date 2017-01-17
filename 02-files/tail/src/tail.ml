de042c55a7a8788a42b2927599619e69504516d0
Henry Till <henrytill@gmail.com>
2017-01-17 18:35:18 -0500

    02-files/tail: add new implementation of tail

--- 02-files/tail/src/tail.ml
+++ 02-files/tail/src/tail.ml
@@ -32,6 +32,60 @@ let lines_alt bytes : bytes list =
   in
   loop (Bytes.length bytes - 2) []
 
+let new_lines bytes : int list =
+  let rec loop start acc =
+    try
+      let next = Bytes.rindex_from bytes start '\n' in
+      if start != next then
+        loop (next - 1) (next :: acc)
+      else
+        loop (next - 1) acc
+    with Not_found ->
+      0 :: acc
+  in
+  let last_byte = Bytes.length bytes - 1 in
+  loop last_byte [last_byte]
+
+let new_lines_count bytes n : int list =
+  let rec loop start counter acc =
+    if counter = 0 then
+      acc
+    else
+      try
+        let next = Bytes.rindex_from bytes start '\n' in
+        loop (next - 1) (counter - 1) (next :: acc)
+      with Not_found ->
+        0 :: acc
+  in
+  let last_byte = Bytes.length bytes - 1 in
+  loop last_byte n [last_byte]
+
+let operate_on_bytes_at_indices source indices k : unit =
+  let rec loop remaining count =
+    match remaining with
+    | []                     -> ()
+    | right :: []            -> ()
+    | 0     :: right :: rest ->
+      k (Bytes.sub source 0 (right + 1)) count;
+      loop (right :: rest) (count + 1)
+    | left  :: right :: rest ->
+      k (Bytes.sub source (left + 1) (right - left)) count;
+      loop (right :: rest) (count + 1)
+  in
+  loop indices 0
+
+let get_bytes_at_indices bytes indices : bytes array =
+  let target_bytes = Array.make ((List.length indices) - 1) Bytes.empty in
+  operate_on_bytes_at_indices bytes indices (fun src cnt -> Array.set target_bytes cnt src);
+  target_bytes
+
+let print_bytes_at_indices bytes indices : unit =
+  operate_on_bytes_at_indices bytes indices (fun src _ -> print_bytes src)
+
+let new_tail bytes count =
+  let indices = new_lines_count bytes count in
+  get_bytes_at_indices bytes indices
+
 let rec drop n xs =
   match xs with
   | [] -> []
