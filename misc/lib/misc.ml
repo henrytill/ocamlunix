abd2529261da575744560253001b653f8f67085f
Henry Till <henrytill@gmail.com>
2017-01-25 06:58:17 -0500

    misc: add {restart_on_EINTR,free_children} and update

--- misc/lib/misc.ml
+++ misc/lib/misc.ml
@@ -3,27 +3,17 @@ let try_finalize f x finally y =
   finally y;
   res
 
-(*
- * A better alternative?
- *
- * http://stackoverflow.com/questions/11276985/emulating-try-with-finally-in-ocaml
- * http://caml.inria.fr/pub/ml-archives/caml-list/2003/07/5ff669a9d2be35ec585b536e2e0fc7ca.en.html
- *
- *)
-let protect ~f ~(finally: unit -> unit) =
-  let result = ref None in
-  try
-    result := Some (f ());
-    raise Exit
-  with
-  | Exit as e ->
-    finally ();
-    (match !result with Some x -> x | None -> raise e)
-  | e ->
-    finally (); raise e
-
 let iter_dir f dirname =
   let open Unix in
   let d = opendir dirname in
   try while true do f (readdir d) done
   with End_of_file -> closedir d
+
+let rec restart_on_EINTR f x =
+  let open Unix in
+  try f x with Unix_error (EINTR, _, _) -> restart_on_EINTR f x
+
+let free_children _ =
+  let open Unix in
+  try while fst (waitpid [ WNOHANG ] (-1)) > 0 do () done
+  with Unix_error (ECHILD, _, _) -> ()
--- misc/lib/misc.mli
+++ misc/lib/misc.mli
@@ -1,5 +1,17 @@
+(** Miscellaneous functions from
+    {{: https://ocaml.github.io/ocamlunix/ocamlunix.html }
+     {i Unix system programming in OCaml }}. *)
+
 val try_finalize : ('a -> 'b) -> 'a -> ('c -> 'd) -> 'c -> 'b
 
-val protect : f:(unit -> 'a) -> finally:(unit -> unit) -> 'a
-
 val iter_dir : (bytes -> 'a) -> bytes -> unit
+
+val restart_on_EINTR : ('a -> 'b) -> 'a -> 'b
+(** Repeat a system call when it is interrupted by a signal, i.e. when the
+    [EINTR] exception is raised *)
+
+val free_children : 'a -> unit
+(** Executes [waitpid] in non-blocking mode (option [WNOHANG]) to recover any
+    dead children and repeats until either there are only live children (zero is
+    returned instead of a child id) or there are no children ([ECHILD]
+    exception) *)
