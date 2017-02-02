717a687048c7d2e40080c956b804f93d1fe755de
Henry Till <henrytill@gmail.com>
2017-02-01 23:34:42 -0500

    misc: open Unix at top of file

--- misc/lib/misc.ml
+++ misc/lib/misc.ml
@@ -1,19 +1,18 @@
+open Unix
+
 let try_finalize f x finally y =
   let res = try f x with exn -> finally y; raise exn in
   finally y;
   res
 
 let iter_dir f dirname =
-  let open Unix in
   let d = opendir dirname in
   try while true do f (readdir d) done
   with End_of_file -> closedir d
 
 let rec restart_on_EINTR f x =
-  let open Unix in
   try f x with Unix_error (EINTR, _, _) -> restart_on_EINTR f x
 
 let free_children _ =
-  let open Unix in
   try while fst (waitpid [ WNOHANG ] (-1)) > 0 do () done
   with Unix_error (ECHILD, _, _) -> ()
