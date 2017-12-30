2e0d2c9f1ce9818d98e414ec6fd7f96aa5dd08dd
Henry Till <henrytill@gmail.com>
2017-12-29 20:53:34 -0500

    05-pipes/single_write: refactor

---
+++ 05-pipes/single_write/lib/ex.h
@@ -0,0 +1,8 @@
+#ifndef EX_H
+#define EX_H
+
+#include <caml/mlvalues.h>
+
+CAMLprim value caml_single_write (value, value, value, value);
+
+#endif
--- 05-pipes/single_write/myocamlbuild.ml
+++ 05-pipes/single_write/myocamlbuild.ml
@@ -1,10 +1,22 @@
-open Ocamlbuild_plugin;;
+open Ocamlbuild_plugin
 
 (*
- * http://stackoverflow.com/questions/19644451/how-to-make-ocamlbuild-compile-and-link-a-c-file-into-an-ocaml-project#19662145
- * http://l-lang.blogspot.de/2012/08/incorporating-c-code-in-ocaml-project.html
+ * https://stackoverflow.com/questions/19644451/how-to-make-ocamlbuild-compile-and-link-a-c-file-into-an-ocaml-project
+ * https://ocaml.org/learn/tutorials/ocamlbuild/Ocamlbuild_example_with_C_stubs.html
  *)
 
-dispatch (function
-    | After_rules -> pdep ["link"] "linkdep" (fun param -> [param])
-    | _           -> ())
+let headers = ["lib/ex.h"]
+let static  = true
+
+let () =
+  dispatch begin
+    function
+    | After_rules ->
+        flag ["link"; "library"; "ocaml"; "byte";   "use_ex"] (S [A "-dllib"; A "-lex"; A "-cclib"; A "-lex"]);
+        flag ["link"; "library"; "ocaml"; "native"; "use_ex"] (S [A "-cclib"; A "-lex"]);
+        if static then flag ["link"; "ocaml"; "byte"] (A "-custom");
+        dep ["link"; "ocaml"; "byte";   "use_ex"] ["lib/dllex.so"; "lib/libex.a"];
+        dep ["link"; "ocaml"; "native"; "use_ex"] ["lib/libex.a"];
+        dep ["compile"; "c"] headers;
+    | _ ->  ()
+  end
