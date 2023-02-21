f1f6fe2287164072db2c79fafc4ae18abf29da79
Henry Till <henrytill@gmail.com>
2023-02-21 02:31:44 -0800

    release.nix: tweak

--- release.nix
+++ release.nix
@@ -1,19 +1,19 @@
 { pkgs ? import <nixpkgs> {}
+, system ? builtins.currentSystem
 , compiler ? "4.14.1"
 }:
 
 let
-  lib = pkgs.lib;
   opam-nix = import (builtins.fetchGit {
     url = "https://github.com/tweag/opam-nix.git";
     rev = "15f724c3e01066fdb41f9ae7e3123f26a65b1d33";
   });
-  on = opam-nix.lib.${builtins.currentSystem};
+  on = opam-nix.lib.${system};
   args = {
     inherit pkgs;
     resolveArgs.with-test = true;
   };
-  src = lib.cleanSource ./.;
+  src = pkgs.lib.cleanSource ./.;
   query = {
     ocaml-base-compiler = compiler;
   };
