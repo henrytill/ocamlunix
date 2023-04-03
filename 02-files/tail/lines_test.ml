f276ae6b7bbc65beaf2518fd7badef2cb15a20c5
Henry Till <henrytill@gmail.com>
2023-04-03 23:56:18 +0000

    lines_test: tweak

--- 02-files/tail/lines_test.ml
+++ 02-files/tail/lines_test.ml
@@ -3,44 +3,47 @@ module type Lines_impl = sig
 end
 
 module Make (M : Lines_impl) : Common.Tests = struct
-  let test_bytes : string =
-    "this is the first line\nthis is the second line\nthis is the third line\n"
+  let test_bytes : bytes =
+    Bytes.of_string
+      "this is the first line\nthis is the second line\nthis is the third line\n"
   ;;
 
-  let expected_test_bytes : string array =
-    [| "this is the first line\n"
-     ; "this is the second line\n"
-     ; "this is the third line\n"
-    |]
+  let expected_test_bytes : bytes array =
+    Array.map
+      Bytes.of_string
+      [| "this is the first line\n"
+       ; "this is the second line\n"
+       ; "this is the third line\n"
+      |]
   ;;
 
-  let test_bytes_no_trailing_newline : string =
-    "this is the first line\n\
-     this is the second line\n\
-     this is the third line\n\
-     this is the fourth line"
+  let test_bytes_no_trailing_newline : bytes =
+    Bytes.of_string
+      "this is the first line\n\
+       this is the second line\n\
+       this is the third line\n\
+       this is the fourth line"
   ;;
 
-  let expected_test_bytes_no_trailing_newline : string array =
-    [| "this is the first line\n"
-     ; "this is the second line\n"
-     ; "this is the third line\n"
-     ; "this is the fourth line"
-    |]
+  let expected_test_bytes_no_trailing_newline : bytes array =
+    Array.map
+      Bytes.of_string
+      [| "this is the first line\n"
+       ; "this is the second line\n"
+       ; "this is the third line\n"
+       ; "this is the fourth line"
+      |]
   ;;
 
   let test_bytes_test () =
-    Alcotest.(check (array bytes))
-      "same elements"
-      (Array.map Bytes.of_string expected_test_bytes)
-      (M.f (Bytes.of_string test_bytes))
+    Alcotest.(check (array bytes)) "same elements" expected_test_bytes (M.f test_bytes)
   ;;
 
   let test_bytes_no_trailing_newline_test () =
     Alcotest.(check (array bytes))
       "same elements"
-      (Array.map Bytes.of_string expected_test_bytes_no_trailing_newline)
-      (M.f (Bytes.of_string test_bytes_no_trailing_newline))
+      expected_test_bytes_no_trailing_newline
+      (M.f test_bytes_no_trailing_newline)
   ;;
 
   let test_set =
