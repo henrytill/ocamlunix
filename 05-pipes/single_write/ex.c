4eddcd64ccb60a8da2ef76d25754712253077956
Henry Till <henrytill@gmail.com>
2023-08-02 23:11:10 -0700

    05-pipes/single_write: tweak

--- 05-pipes/single_write/ex.c
+++ 05-pipes/single_write/ex.c
@@ -1,9 +1,10 @@
+#include <errno.h>
+#include <string.h>
+
 #include <caml/memory.h>
 #include <caml/mlvalues.h>
 #include <caml/signals.h>
 #include <caml/unixsupport.h>
-#include <errno.h>
-#include <string.h>
 
 #include "ex.h"
 
@@ -17,14 +18,17 @@ CAMLprim value caml_single_write(value fd, value buf, value vofs, value vlen)
 	ofs = Long_val(vofs);
 	len = Long_val(vlen);
 	ret = 0;
+
 	if (len > 0) {
 		numbytes = len > UNIX_BUFFER_SIZE ? UNIX_BUFFER_SIZE : len;
 		memmove(iobuf, &Byte(buf, ofs), numbytes);
 		caml_enter_blocking_section();
 		ret = write(Int_val(fd), iobuf, numbytes);
 		caml_leave_blocking_section();
-		if (ret == -1)
+		if (ret == -1) {
 			uerror("single_write", Nothing);
+		}
 	}
+
 	CAMLreturn(Val_int(ret));
 }
