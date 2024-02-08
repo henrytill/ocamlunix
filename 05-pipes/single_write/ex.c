1ffc2e7ad5c293ce744f3ebf7bf17e60ccd9d083
Henry Till <henrytill@gmail.com>
2024-02-07 21:21:11 -0800

    update .clang-format and reformat

--- 05-pipes/single_write/ex.c
+++ 05-pipes/single_write/ex.c
@@ -8,26 +8,27 @@
 
 #include "ex.h"
 
-CAMLprim value caml_single_write(value fd, value buf, value vofs, value vlen) {
-  CAMLparam4(fd, buf, vofs, vlen);
-  long ofs, len;
-  int numbytes, ret;
-  char iobuf[UNIX_BUFFER_SIZE];
+CAMLprim value caml_single_write(value fd, value buf, value vofs, value vlen)
+{
+    CAMLparam4(fd, buf, vofs, vlen);
+    long ofs, len;
+    int numbytes, ret;
+    char iobuf[UNIX_BUFFER_SIZE];
 
-  ofs = Long_val(vofs);
-  len = Long_val(vlen);
-  ret = 0;
+    ofs = Long_val(vofs);
+    len = Long_val(vlen);
+    ret = 0;
 
-  if (len > 0) {
-    numbytes = len > UNIX_BUFFER_SIZE ? UNIX_BUFFER_SIZE : len;
-    memmove(iobuf, &Byte(buf, ofs), numbytes);
-    caml_enter_blocking_section();
-    ret = write(Int_val(fd), iobuf, numbytes);
-    caml_leave_blocking_section();
-    if (ret == -1) {
-      uerror("single_write", Nothing);
+    if (len > 0) {
+        numbytes = len > UNIX_BUFFER_SIZE ? UNIX_BUFFER_SIZE : len;
+        memmove(iobuf, &Byte(buf, ofs), numbytes);
+        caml_enter_blocking_section();
+        ret = write(Int_val(fd), iobuf, numbytes);
+        caml_leave_blocking_section();
+        if (ret == -1) {
+            uerror("single_write", Nothing);
+        }
     }
-  }
 
-  CAMLreturn(Val_int(ret));
+    CAMLreturn(Val_int(ret));
 }
