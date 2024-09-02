35b88050d360d352f17728b876836ddaa2e57759
Henry Till <henrytill@gmail.com>
2024-09-01 18:52:45 -0700

    {default,flake}.nix: reformat

--- flake.nix
+++ flake.nix
@@ -12,21 +12,38 @@
     };
   };
 
-  outputs = { self, flake-utils, opam-nix, nixpkgs, ... }@inputs:
+  outputs =
+    {
+      self,
+      flake-utils,
+      opam-nix,
+      nixpkgs,
+      ...
+    }@inputs:
     let
       package = "ocamlunix";
     in
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
-        args = { resolveArgs.with-test = true; };
-        src = builtins.path { path = ./.; name = "ocamlunix-src"; };
-        query = { ocaml-base-compiler = "4.14.1"; };
+        args = {
+          resolveArgs.with-test = true;
+        };
+        src = builtins.path {
+          path = ./.;
+          name = "ocamlunix-src";
+        };
+        query = {
+          ocaml-base-compiler = "4.14.1";
+        };
         scope = on.buildDuneProject args package src query;
-        overlay = final: prev: {};
-      in {
+        overlay = final: prev: { };
+      in
+      {
         legacyPackages = scope.overrideScope' overlay;
         packages.default = self.legacyPackages.${system}.${package};
-      });
+      }
+    );
 }
